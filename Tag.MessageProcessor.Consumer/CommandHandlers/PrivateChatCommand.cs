7fcad5d2e105da7a7508e9a172b83b41042f3207
Artem Kiyashko <artem86@hotmail.co.uk>
2024-10-11 14:14:54 +0200

    Typofix

--- Tag.MessageProcessor.Consumer/CommandHandlers/PrivateChatCommand.cs
+++ Tag.MessageProcessor.Consumer/CommandHandlers/PrivateChatCommand.cs
@@ -8,5 +8,5 @@ public class PrivateChatCommand(ITelegramBotClient telegramBotClient) : ICommand
     private readonly ITelegramBotClient _telegramBotClient = telegramBotClient;
 
     public Task ProcessCommand(TgData tgData) 
-        => _telegramBotClient.SendTextMessageAsync(tgData.ChatTgId, "Добавть меня в групповой чат и выдай разрешение на упраление чатом, чтобы я мог менять аватарки :)");
+        => _telegramBotClient.SendTextMessageAsync(tgData.ChatTgId, "Добавь меня в групповой чат и выдай разрешение на упраление чатом, чтобы я мог менять аватарки :)");
 }
